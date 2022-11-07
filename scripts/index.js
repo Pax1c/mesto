bf6f577c96e9b32696e94650bcd297aa5b06d7ee
Grigory Opleukhin <opgrishaa@yandex.ru>
2022-11-07 20:15:44 +0300

    mesto v2.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,22 @@
-const editButton = document.querySelector('.profile__edit-btn');
-const popupElem = document.querySelector('.popup');
-const popupClose = document.querySelector('.popup__close');
-
-const Open = (popup) => {
-    popupElem.classList.add('popup_status_opened');
-}
-
-const Close = (popup) => {
-    popupElem.classList.remove('popup_status_opened');
-}
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-btn');
+let popupCloseButton = document.querySelector('.popup__close');
 
 editButton.addEventListener('click', () => {
-    Open(popupElem);
+    popup.classList.add('popup_opened');
 });
 
-popupElem.addEventListener('click', (event) => {
-    const isOverlay = event.target.classList.contains('popup_status_opened');
-    const isClose = event.target.classList.contains('popup__close');
-    if (isOverlay || isClose) {
-        Close(popupElem);
-    }
+popupCloseButton.addEventListener('click', () => {
+    popup.classList.remove('popup_opened');
 });
 
-const profileEditBtn = document.querySelector('.popup__form-button');
+let formElem = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__form-item_edit_title');
+let jobInput = document.querySelector('.popup__form-item_edit_subtitle');
 
-profileEditBtn.addEventListener('click', () => {
-    let profileEditTitle = document.querySelector('.popup__form-item_edit_title').value;
-    document.querySelector('.profile__title').innerText = profileEditTitle;
-    let profileEditSubtitle = document.querySelector('.popup__form-item_edit_subtitle').value;
-    document.querySelector('.profile__subtitle').innerText = profileEditSubtitle;
+formElem.addEventListener('submit', (event) => {
+    event.preventDefault();
+    document.querySelector('.profile__title').textContent = nameInput.value;
+    document.querySelector('.profile__subtitle').textContent = jobInput.value;
+    popup.classList.remove('popup_opened');
 });
-const likeBtns = document.querySelectorAll('.element__like-btn');
-
-likeBtns.forEach(likeBtn => {
-    likeBtn.addEventListener('click', (likeAdd) => {
-        likeBtn.classList.toggle('element__like-btn_status_active');
-    })
-})
