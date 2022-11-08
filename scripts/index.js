b1873c2d9951e6d34019dc80863daf4fc8d709e4
Pax1c <112660388+Pax1c@users.noreply.github.com>
2022-11-08 15:19:07 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,8 @@ let editButton = document.querySelector('.profile__edit-btn');
 let popupCloseButton = document.querySelector('.popup__close');
 
 editButton.addEventListener('click', () => {
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
     popup.classList.add('popup_opened');
 });
 
@@ -15,8 +17,6 @@ let profileSubtitle = document.querySelector('.profile__subtitle');
 let formElem = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__form-item_edit_title');
 let jobInput = document.querySelector('.popup__form-item_edit_subtitle');
-nameInput.value = profileTitle.textContent;
-jobInput.value = profileSubtitle.textContent;
 
 formElem.addEventListener('submit', (event) => {
     event.preventDefault();
