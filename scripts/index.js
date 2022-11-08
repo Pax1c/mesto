81c1c7f07ad3c4bbdecb94305852e09593ef551c
Pax1c <112660388+Pax1c@users.noreply.github.com>
2022-11-08 15:27:03 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-btn');
 let popupCloseButton = document.querySelector('.popup__close');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let formElem = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__form-item_edit_title');
+let jobInput = document.querySelector('.popup__form-item_edit_subtitle');
 
 editButton.addEventListener('click', () => {
     nameInput.value = profileTitle.textContent;
@@ -11,12 +16,6 @@ editButton.addEventListener('click', () => {
 popupCloseButton.addEventListener('click', () => {
     popup.classList.remove('popup_opened');
 });
-
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-let formElem = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__form-item_edit_title');
-let jobInput = document.querySelector('.popup__form-item_edit_subtitle');
 
 formElem.addEventListener('submit', (event) => {
     event.preventDefault();
