520b3d0d18ddead56261196a97872662534371f2
Pax1c <112660388+Pax1c@users.noreply.github.com>
2022-12-07 10:57:25 +0300

    feature: добавить валидацию форм

--- scripts/index.js
+++ scripts/index.js
@@ -70,12 +70,42 @@ initialCards.forEach((card) => {
 });
 
 //Добавляем обработчики событий
-buttonEdit.addEventListener('click', () => {
-    openPopup(profilePopup);
+profilePopup.addEventListener('click', (evt) => {
+    const isOverlay = evt.target.classList.contains('popup_opened');
+    const isCloseBtn = evt.target.classList.contains('popup__close');
+    if (isOverlay || isCloseBtn) {
+        closePopup(profilePopup);
+    }
 });
 
-profilePopupCloseBtn.addEventListener('click', () => {
-    closePopup(profilePopup);
+cardPopup.addEventListener('click', (evt) => {
+    const isOverlay = evt.target.classList.contains('popup_opened');
+    const isCloseBtn = evt.target.classList.contains('popup__close');
+    if (isOverlay || isCloseBtn) {
+        closePopup(cardPopup);
+    }
+});
+
+photoPopup.addEventListener('click', (evt) => {
+    const isOverlay = evt.target.classList.contains('popup_opened');
+    const isCloseBtn = evt.target.classList.contains('popup__close');
+    if (isOverlay || isCloseBtn) {
+        closePopup(photoPopup);
+    }
+});
+
+document.addEventListener('keydown', keyClosePopup);
+
+function keyClosePopup(evt) {
+    if (evt.key === "Escape") {
+        closePopup(profilePopup);
+        closePopup(cardPopup);
+        closePopup(photoPopup);
+    }
+};
+
+buttonEdit.addEventListener('click', () => {
+    openPopup(profilePopup);
 });
 
 handleSubmitProfileForm.addEventListener('submit', (event) => {
@@ -89,10 +119,6 @@ buttonAdd.addEventListener('click', () => {
     openPopup(cardPopup);
 });
 
-popupCardCloseBtn.addEventListener('click', () => {
-    closePopup(cardPopup);
-});
-
 handleSubmitCardForm.addEventListener('submit', (event) => {
     event.preventDefault();
     renderCard({ name: nameCardInput.value, link: imageCardInput.value });
@@ -101,4 +127,160 @@ handleSubmitCardForm.addEventListener('submit', (event) => {
     closePopup(cardPopup);
 });
 
-photoPopupCloseBtn.addEventListener('click', () => closePopup(photoPopup));
+
+//Валидация
+const profileForm = document.querySelector('#profile_form');
+const profileValidators = {
+    name: validateName,
+    job: validateJob,
+
+};
+enableValidation(profileForm, profileValidators);
+
+const cardForm = document.querySelector('#card_form');
+const cardValidators = {
+    place: validatePlace,
+    link: validateLink
+};
+enableValidation(cardForm, cardValidators);
+
+function enableValidation(form, validators) {
+
+    function validate(key, value) {
+        const validator = validators[key];
+        return validator(value);
+    };
+
+    form.addEventListener('input', (evt) => {
+        const key = evt.target.name;
+        const value = evt.target.value
+        const formData = new FormData(evt.currentTarget);
+        const values = Object.fromEntries(formData);
+
+        const errorMessage = validate(key, value)
+
+        if (!errorMessage) {
+            evt.target.onblur = () => {
+                evt.target.dataset.dirty = 'true';
+            };
+            clearError(key);
+            return;
+        }
+
+        // есть ошибка
+        if (evt.target.dataset.dirty === 'true') {
+            setError(key, errorMessage);
+            return;
+        }
+
+        // есть ошибка, но мы еще не ушли с поля
+        evt.target.onblur = () => {
+            evt.target.dataset.dirty = 'true';
+            setError(key, errorMessage);
+        };
+    });
+
+    function setError(key, errorMessage) {
+        const inputElement = form.querySelector(`.popup__form-item[name=${key}]`);
+        let errorElement = form.querySelector(`.popup__form-item_type_error[data-key=${key}]`);
+        const buttonElement = form.querySelector('.popup__form-button');
+        buttonElement.classList.add('popup__form-button_inactive');
+        buttonElement.disabled = true;
+
+        if (!errorElement) {
+            errorElement = document.createElement('p');
+            inputElement.after(errorElement);
+        }
+
+        inputElement.classList.add('popup__form-item_invalid');
+        errorElement.classList.add('popup__form-item_type_error');
+        errorElement.dataset.key = key;
+        errorElement.textContent = errorMessage;
+    };
+
+    function clearError(key) {
+        const inputElement = form.querySelector(`.popup__form-item[name=${key}]`);
+        inputElement.classList.remove('popup__form-item_type_error');
+        inputElement.classList.remove('popup__form-item_invalid');
+        const buttonElement = form.querySelector('.popup__form-button');
+        buttonElement.classList.remove('popup__form-button_inactive');
+        buttonElement.disabled = false;
+        const errorElement = form.querySelector(`.popup__form-item_type_error[data-key=${key}]`);
+        if (errorElement) {
+            errorElement.remove();
+        }
+    };
+
+    form.addEventListener('submit', (evt) => {
+        const formData = new FormData(evt.currentTarget);
+        const values = Object.fromEntries(formData);
+
+        let isFormValid = true;
+
+        formData.forEach((value, key) => {
+            const errorMessage = validate(key, value);
+
+            if (!errorMessage) {
+                return;
+            }
+
+            setError(key, errorMessage);
+            const inputElement = form.querySelector(`.popup__form-item[name=${key}]`);
+            inputElement.dataset.dirty = 'true';
+            isFormValid = false;
+        });
+
+        if (!isFormValid) {
+            evt.preventDefault();
+            return;
+        }
+    });
+}
+
+//Validators
+function validateName(value) {
+    if (!value) {
+        return 'Вы не заполнили поле';
+    }
+    if ((value.length < 2) || (value.length > 40)) {
+        return 'Длина поля "Имя" должна быть не менее 2 символов и не более 40 символов';
+    } else {
+        return null;
+    }
+};
+
+function validateJob(value) {
+    if (!value) {
+        return 'Вы не заполнили поле';
+    }
+    if ((value.length < 2) || (value.length > 200)) {
+        return 'Длина поля "О себе" должна быть не менее 2 символов и не более 200 символов';
+    } else {
+        return null;
+    }
+
+};
+
+function validatePlace(value) {
+    if (!value) {
+        return 'Вы не заполнили поле';
+    }
+    if ((value.length < 2) || (value.length > 30)) {
+        return 'Длина поля "Название" должна быть не менее 2 символов и не более 30 символов';
+    } else {
+        return null;
+    }
+};
+
+function validateLink(url) {
+    const obj = /(^https?:\/\/)?[a-z0-9~_\-\.]+\.[a-z]{2,9}(\/|:|\?[!-~]*)?$/i;
+    if (!url) {
+        return 'Вы не заполнили поле';
+    }
+    if (!obj.test(url)) {
+        return 'Некорректная ссылка.';
+    }
+    else {
+        return null;
+    }
+};
